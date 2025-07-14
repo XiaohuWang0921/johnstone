6d6eda8e491043aa4761c695498b60714556e673
Xiaohu Wang <xiaohu.wang0921@gmail.com>
2025-07-14 21:21:58 +0200

    update for lean4

--- johnstone.lean
+++ johnstone.lean
@@ -1,93 +1,74 @@
-universes u v
-
-def monic {α : Type u} {β : Type v} (f : α → β) :=
+def monic (f : α → β) :=
   ∀ x y, f x = f y → x = y
 
-section johnstone
+def johnstone (φ : Prop → Prop) (μ : monic φ) (p : Prop) : φ (φ p) = p :=
+  let UT := {q // φ q}
 
-open true
+  let UP := ∃ q, φ q
 
-variables
-  (p : Prop)
-  (φ : Prop → Prop)
-  (μ : monic φ)
+  let VP := ∃ q, φ q ∧ q
 
-theorem johnstone : φ (φ p) = p :=
-  let
-    UT := {q // φ q},
-    VT := {q // φ q ∧ q},
-    UP := ∃ q, φ q,
-    VP := ∃ q, φ q ∧ q,
+  have h₀ : φ True → VP :=
+    fun w => ⟨True, w, trivial⟩
 
-    h₀ (w : φ true) : VP :=
-      ⟨true, w, intro⟩,
+  have h₁ : VP → φ True :=
+    fun ⟨_, w₀, w₁⟩ =>
+      (propext ⟨fun _ => trivial, fun _ => w₁⟩).rec w₀
 
-    h₁ : VP → φ true :=
-      λ ⟨_, w₀, w₁⟩,
-        eq.rec w₀ $
-          propext ⟨(λ _, intro), (λ _, w₁)⟩,
-  
-    h₂ : φ true = VP :=
-      propext ⟨h₀, h₁⟩,
-  
-    h₃ : ∀ t : UT, t.val → VP :=
-      λ ⟨q, w₀⟩ w₁, ⟨q, w₀, w₁⟩,
-  
-    h₄ : ∀ t : UT, VP → t.val :=
-      λ ⟨q₀, w₀⟩ ⟨q₁, w₁, w₂⟩,
-        @eq.rec Prop q₁ id w₂ q₀ $
-          μ q₁ q₀ $
-            propext ⟨(λ _, w₀), (λ _, w₁)⟩,
-  
-    h₅ (t : UT) : t.val = VP :=
-      propext ⟨h₃ t, h₄ t⟩,
-  
-    h₆ (w : φ VP) : UP :=
-      ⟨VP, w⟩,
-  
-    h₇ : UP → φ VP :=
-      λ ⟨q, w⟩,
-        eq.rec w $
-          h₅ ⟨q, w⟩,
-  
-    h₈ : φ VP = UP :=
-      propext ⟨h₆, h₇⟩,
-  
-    h₉ : ∀ t : UT, t.val → t.val = UP :=
-      λ ⟨q, w₀⟩ w₁,
-        propext ⟨(λ _, ⟨q, w₀⟩), (λ _, w₁)⟩,
-  
-    h₁₀ (w : φ true) : φ UP :=
-      eq.rec w $
-        h₉ ⟨true, w⟩ intro,
-  
-    h₁₁ (w : φ UP) : φ true :=
-      h₁ ⟨UP, w, UP, w⟩,
-  
-    h₁₂ : φ UP = φ true :=
-      propext ⟨h₁₁, h₁₀⟩,
-  
-    h₁₃ : UP = true :=
-      μ UP true h₁₂,
-  
-    h₁₄ : φ (φ true) = true :=
-      trans (congr (eq.refl φ) h₂) $
-        trans h₈ h₁₃,
-  
-    h₁₅ (w : φ $ φ p) : p :=
-      @eq.rec Prop true id intro p $
-        μ true p $
-          μ (φ true) (φ p) $
-            trans h₁₄ $
-              propext ⟨(λ _, w), (λ _, intro)⟩,
-  
-    h₁₆ (w : p) : φ $ φ p :=
-      @eq.rec Prop true id intro (φ $ φ p) $
-        trans (symm h₁₄) $
-          congr (eq.refl φ) $
-            congr (eq.refl φ) $
-              propext ⟨(λ _, w), (λ _, intro)⟩
-  in
-    propext ⟨h₁₅, h₁₆⟩
+  have h₂ : φ True = VP :=
+    propext ⟨h₀, h₁⟩
 
-end johnstone
+  have h₃ : ∀ t : UT, t.val → VP :=
+    fun ⟨q, w₀⟩ w₁ => ⟨q, w₀, w₁⟩
+
+  have h₄ : ∀ t : UT, VP → t.val :=
+    fun ⟨q₀, w₀⟩ ⟨q₁, w₁, w₂⟩ =>
+      have l₀ : φ q₁ = φ q₀ := propext ⟨fun _ => w₀, fun _ => w₁⟩
+      have l₁ : q₁ = q₀ := μ q₁ q₀ l₀
+      @l₁.rec Prop q₁ (fun a _ => a) w₂
+
+  have h₅ : ∀ t : UT, t.val = VP :=
+    fun t => propext ⟨h₃ t, h₄ t⟩
+
+  have h₆ : φ VP → UP := fun w => ⟨VP, w⟩
+
+  have h₇ : UP → φ VP :=
+    fun ⟨q, w⟩ => (h₅ ⟨q, w⟩).rec w
+
+  have h₈ : φ VP = UP :=
+    propext ⟨h₆, h₇⟩
+
+  have h₉ : ∀ t : UT, t.val → t.val = UP :=
+      fun ⟨q, w₀⟩ w₁ =>
+        propext ⟨fun _ => ⟨q, w₀⟩, fun _ => w₁⟩
+
+  have h₁₀ : φ True → φ UP :=
+    fun w => (h₉ ⟨True, w⟩ trivial).rec w
+
+  have h₁₁ : φ UP → φ True :=
+    fun w => h₁ ⟨UP, w, UP, w⟩
+
+  have h₁₂ : φ UP = φ True :=
+    propext ⟨h₁₁, h₁₀⟩
+
+  have h₁₃ : UP = True :=
+    μ UP True h₁₂
+
+  have h₁₄ : φ (φ True) = True :=
+    (congr (Eq.refl φ) h₂).trans (h₈.trans h₁₃)
+
+  have h₁₅ : φ (φ p) → p :=
+    fun w =>
+      have l₀ : True = φ (φ p) := propext ⟨fun _ => w, fun _ => trivial⟩
+      have l₁ : φ (φ True) = φ (φ p) := trans h₁₄ l₀
+      have l₂ : True = p := μ True p <| μ (φ True) (φ p) l₁
+      l₂.rec trivial
+
+  have h₁₆ : p → φ (φ p) :=
+    fun w =>
+      have l₀ : True = p := propext ⟨fun _ => w, fun _ => trivial⟩
+      have l₁ : (φ ∘ φ) True = (φ ∘ φ) p := congr (Eq.refl <| φ ∘ φ) l₀
+      have l₂ : True = φ (φ p) := Eq.trans (Eq.symm h₁₄) l₁
+      l₂.rec trivial
+
+  propext ⟨h₁₅, h₁₆⟩
